6e2d756e430d3f6206e5fe116688c5ec017a810a
Anastasiia Kazymko <92369437+AnastasiiaKazymko@users.noreply.github.com>
2022-07-01 21:38:58 +0300

    fix HW

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -5,10 +5,10 @@ function randomColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-export const Statistics = ({ stats }) => {    
+export const Statistics = ({ title, stats }) => {    
       return (
     <section className={css.statistics}>
-        <h2 className={css.title}>Upload stats</h2>
+        {title && <h2 className={css.title}>{title}</h2>}
 
           <ul className={css.list}>
             {stats.map(({ id, label, percentage }) => (            
